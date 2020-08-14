e18fdabc313a5b69b5210baa6a4acd72c39b7f22
Laura <viglionigr@gmail.com>
2020-08-14 17:55:33 -0300

    fix: fixed functions order

--- src/stocks/controller/health_check.clj
+++ src/stocks/controller/health_check.clj
@@ -1,10 +1,11 @@
 (ns stocks.controller.health-check
   (:require [ring.util.http-response :refer [ok]]))
 
-(defn health-check []
-  (ok (str "It's working! It's working!\n" (date-and-time) "\n")))
 
 (defn date-and-time []
   (java.util.TimeZone/setDefault
    (java.util.TimeZone/getTimeZone "America/Sao_Paulo"))
   (.toString (java.util.Date.)))
+
+(defn health-check []
+  (ok (str "It's working! It's working!\n" (date-and-time) "\n")))
