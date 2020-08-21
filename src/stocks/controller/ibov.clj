8013a9ac5aadd435027260451bf0565b48a7455d
Laura <viglionigr@gmail.com>
2020-08-20 22:54:08 -0300

    fix: added error handling to exception in curl

--- src/stocks/controller/ibov.clj
+++ src/stocks/controller/ibov.clj
@@ -15,14 +15,18 @@
 
 
 (defn stock-info [stock-id]
-  (let [page (stock-page stock-id)]
-    (if (logic/stock-not-found page)
-      (resp/not-found "Lost a stock Master Obi-Wan has. How embarrassing.\n")
-      (resp/ok {:price (logic/stock-price page)
-                :max (logic/day-max page)
-                :min (logic/day-min page)
-                :variation (logic/variation page)
-                :stock-id stock-id
-                :currency "brl"
-                :time (date-and-time-js)}))))
+  (try
+    (let [page (stock-page stock-id)]
+      (if (logic/stock-not-found page)
+        (resp/not-found "Lost a stock Master Obi-Wan has. How embarrassing.\n")
+        (resp/ok {:price (logic/stock-price page)
+                  :max (logic/day-max page)
+                  :min (logic/day-min page)
+                  :variation (logic/variation page)
+                  :stock-id stock-id
+                  :currency "brl"
+                  :time (date-and-time-js)})))
+    (catch Exception e
+      ;; (println (str "caught exception: " (.getMessage e)))
+      (resp/internal-server-error "The dark side clouds everything. Impossible to see this stock is."))))
 
